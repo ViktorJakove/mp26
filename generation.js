26b7308acdac1a2b2d7ddb8301f954007be77c7e
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2025-11-18 16:59:37 +0100

    indefinite grid move

--- generation.js
+++ generation.js
@@ -17,14 +17,20 @@ document.documentElement.style.overflow = "hidden";
 const cellSize = 50;
 const grid = new PIXI.Graphics();
 grid.lineStyle(1, 0xcccccc);
-for (let x = 0; x < app.screen.width; x += cellSize) {
-    grid.moveTo(x, 0);
-    grid.lineTo(x, app.screen.height);
-}for (let y = 0; y < app.screen.height; y += cellSize) {
-    grid.moveTo(0, y);
-    grid.lineTo(app.screen.width, y);
-}
 app.stage.addChild(grid);
+
+//grid updated draw
+function drawGrid(offsetX, offsetY) {
+    grid.clear();
+    grid.lineStyle(1, 0xcccccc);
+    for (let x = offsetX % cellSize; x < app.screen.width; x += cellSize) {
+        grid.moveTo(x, 0);
+        grid.lineTo(x, app.screen.height);
+    }for (let y = offsetY % cellSize; y < app.screen.height; y += cellSize) {
+        grid.moveTo(0, y);
+        grid.lineTo(app.screen.width, y);
+    }
+}
 
 //cities defs and render
 const cities = {
@@ -43,4 +49,34 @@ Object.values(cities).forEach(city => {
     cityText.style.align = 'center';
     app.stage.addChild(cityText);
 });
+//
 console.log(cities[0]);
+//
+
+//mouse move
+let isDragging = false;
+let mouseInitialPos = { x: 0, y: 0 };
+let offset  = { x: 0, y: 0 };
+
+app.view.addEventListener('mousedown', (event) => {
+    isDragging = true;
+    mouseInitialPos = { x: event.clientX, y: event.clientY };
+});
+
+app.view.addEventListener('mouseup', () => {
+    isDragging = false;
+});
+
+app.view.addEventListener('mousemove', (event) => {
+    if (isDragging) {
+        const dx = event.clientX - mouseInitialPos.x;
+        const dy = event.clientY - mouseInitialPos.y;
+        offset.x += dx;
+        offset.y += dy;
+        mouseInitialPos = { x: event.clientX, y: event.clientY };
+
+        drawGrid(offset.x, offset.y);
+        cityGridGraphics.x = offset.x;
+        cityGridGraphics.y = offset.y;
+    }
+});
