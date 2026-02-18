22d3481205767a1ca424a6f8144f7a90ddf80bbc
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2026-02-18 19:29:31 +0100

    yokiuhd

--- utils/colorGenerator.js
+++ utils/colorGenerator.js
@@ -1,9 +1,9 @@
 export class ColorGenerator {
-    constructor(options = {}) {
+    constructor(params = {}) {
         this.hue = Math.random() * 360;
 
-        this.sat = options.sat ?? 0.7;
-        this.light = options.light ?? 0.45;
+        this.sat = params.sat ?? 0.7;
+        this.light = params.light ?? 0.45;
 
         this.goldenAngle = 137.508;
     }
@@ -15,14 +15,14 @@ export class ColorGenerator {
 
     hslToHex(h, s, l) {
 
-        const k = n => (n + h / 30) % 12;
-        const a = s * Math.min(l, 1 - l);
-        const f = n =>
-            l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+        const hueRot = n => (n + h / 30) % 12;
+        const chroma = s * Math.min(l, 1 - l);
+        const channelVal = n =>
+            l - chroma * Math.max(-1, Math.min(hueRot(n) - 3, Math.min(9 - hueRot(n), 1)));
 
-        const r = Math.round(255 * f(0));
-        const g = Math.round(255 * f(8));
-        const b = Math.round(255 * f(4));
+        const r = Math.round(255 * channelVal(0));
+        const g = Math.round(255 * channelVal(8));
+        const b = Math.round(255 * channelVal(4));
 
         return (r << 16) + (g << 8) + b;
     }
