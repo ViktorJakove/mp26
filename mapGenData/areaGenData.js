25bb4cc32769b3298a36243bf0bfb3ce1fb1e96a
ViktorJakove <viktor.jakovec@student.gyarab.cz>
2026-02-22 18:00:15 +0100

    city info

--- mapGenData/areaGenData.js
+++ mapGenData/areaGenData.js
@@ -1,6 +1,6 @@
 export const AREA_GEN_DATA = {
     areaSize: [[30,18],[100,80],[200,200]],
-    cityCount:[4,4,5],
+    cityCount:[5,10,20],
     lakeCount: [1,2,5],
     forestCount: [2,2,0],
     mountainCount: [1,3,0],
